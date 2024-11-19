08c31f40112d520d1910cbad4867f1ddbe5d7e68
Work <Work@MSI>
2024-11-19 16:24:24 -0600

    Where we ended on Nov 19 - resume with more recursion (quicksort, tower of hanoi, etc.)

--- November19_2024-COSC2436/November19_2024-COSC2436.cpp
+++ November19_2024-COSC2436/November19_2024-COSC2436.cpp
@@ -2,14 +2,82 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using std::cout; 
 
+int recursivelyCalculateFactorial(int number)
+{
+	if (number == 1)
+	{
+		return 1; 
+	}
+
+	else
+	{
+		return number * recursivelyCalculateFactorial(number - 1);
+	}
+}
+
+int iterativelyCalculateFactorial(int number) //suppose number == 5
+{
+	int factorial = 1; 
+
+	for (int i = 1; i <= number; i++)
+	{
+		factorial = factorial * i; //fact = 1 * 1
+	}
+
+	return factorial; 
+}
+
+using std::vector; 
+
+int getElementCount(vector<int> nums)
+{
+	if (nums.empty()) //base case
+	{ 
+		return 0;  //this is not a recursive call (getElementCount is not calling itself here) 
+	}
+
+	else //"recursive case" (not upper case) 
+	{
+		nums.pop_back(); 
+		return 1 + getElementCount(nums);
+	}
+	//return nums.size(); 
+}
+
+int recursiveVectorSum(vector<int> nums)
+{
+	if (nums.size() == 1) //base case
+	{
+		return nums.back(); 
+	}
+
+	else
+	{
+		int back = nums.back(); //9 the first time ...
+		nums.pop_back(); 
+		return back + recursiveVectorSum(nums); //nums will be different than it was on the FIRST call of this function 
+	}
+}
+
 int main()
 {
-	cout << "Hello world\n";
+	///cout << recursivelyCalculateFactorial(5) << "\n";
+	//cout << "Iterative factorial of 5 is: " << iterativelyCalculateFactorial(5) << "\n";
+	//cout << "Hello world\n";
 
-	main(); //the main function prints "Hello world" and then calls itself (without end)
+	//while (true)
+	//{
+	//	cout << "Hello\n";
+	//}
+	//main(); //the main function prints "Hello world" and then calls itself (without end)
 	//infinite recursion (results in stack overflow error) 
-	
+
+	vector<int> nums = { 8, 6, 7, 5, 3, 0, 9 }; //sum of values = 38 
+	//cout << getElementCount(nums) << "\n";
+
+	cout << "Is it 38? " << recursiveVectorSum(nums) << "\n";
 }
